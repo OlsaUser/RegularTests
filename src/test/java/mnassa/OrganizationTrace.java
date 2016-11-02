25e152ac2ccb69ae5b46097c41844b9325a3296b
OlsaUser <olga.samotokina@gmail.com>
2016-11-02 16:45:03 +0200

    org - commit1

--- src/test/java/mnassa/OrganizationTrace.java
+++ src/test/java/mnassa/OrganizationTrace.java
@@ -245,6 +245,8 @@ public class OrganizationTrace {
         accountSettingsSteps.enterOrganizationName(OrganizationName_En_exp);
         accountSettingsSteps.enterOrganizationShortName(OrganizationShortName_En_exp);
         accountSettingsSteps.selectTypeGovernmental();
+        JavascriptExecutor jse = (JavascriptExecutor)getDriver();
+        jse.executeScript("window.scrollBy(0,88)", "");
         accountSettingsSteps.selectLanguageArabian_Org();
         accountSettingsSteps.selectLanguageEnglish_Org();
         accountSettingsSteps.selectFounded();
